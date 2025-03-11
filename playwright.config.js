89e01701e9f3d1663ed7c0c7695f530cfd55b33c
lii4ee <angelolife2002@gmail.com>
2025-03-11 21:13:29 +0530

    report

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: 'html',
   use: {
     browserName: 'chromium',
     headless: false,
